135c26bd7dee6fc10b2b534ec283dd0a38489ca1
roshan <roshanshendre24@gmail.com>
2023-04-16 15:46:31 +0530

    changed google app password

--- Charity/settings.py
+++ Charity/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_HOST_USER="roshanshendre24@gmail.com"
-EMAIL_HOST_PASSWORD="nwefwdxzifjqqiig"
+EMAIL_HOST_PASSWORD="akuqmmqhzxrxslvw"
 EMAIL_USE_TLS=True
